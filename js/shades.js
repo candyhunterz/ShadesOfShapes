d032c34410a0562a7c3e949fd284145f4855f3fd
Ycasas-Jan <ycasas.jan@gmail.com>
2015-05-08 10:41:59 -0700

    coppied wins wins

--- js/shades.js
+++ js/shades.js
@@ -5,9 +5,36 @@ $(document).ready(function() {
 	var row1, row2, row3, row4, row5, col1, col2, col3, col4, col5, leftDiag, rightDiag;
 	var correct = 0;
 	var mute = 0;
-
+	var SoundfxNum = "0";
+	var fx1 = new Audio("sounds/fx1.mp3");
+	var fx2 = new Audio("sounds/fx2.mp3");
+	var fx3 = new Audio("sounds/fx3.mp3");
+	var fx4 = new Audio("sounds/fx4.mp3");
+	var fx5 = new Audio("sounds/fx5.mp3");
+	var red = 0;
+	var blue = 0;
+	var yellow = 0;
+	var redClicked = 0;
+	var blueClicked = 0;
+	var yellowClicked = 0;
+	
+	function startGame() {
+		$("#timer").TimeCircles();
+		$("#timer").TimeCircles().start();
+		var delay = 3000; //Your delay in milliseconds
+		setTimeout(function() {
+			window.location = "#bar"; 
+			$("#timer").TimeCircles().destroy();}, delay);
+			for (var i=0; i<board.length; i++) {
+				d3.select("#d"+i).style("opacity", 1).transition().duration(20000).style("opacity", 0);
+		}
+	}
+	
+	$("#playButton").click(function(){startGame()});
+	$("#playAgain").click(function(){startGame()});
+	
 	//function to randomize the colors of the cell
-	function randomize() {
+	function randomize(color,shape) {
 		for (var i=0; i<board.length; i++) {
 			var num = Math.floor(Math.random()*3)
 			var color='';
@@ -30,6 +57,8 @@ $(document).ready(function() {
 	function setColor() {
 		randomize();
 		makeShapes();
+		countColor();
+		console.log("red: " + red + " blue: " + blue + " yellow: " + yellow);
 	}
 	
 	//function to return the color of selection box
@@ -37,37 +66,53 @@ $(document).ready(function() {
 		$("#choice td").css("background-color")
 	}
 
+
 	function reset() {
-		$("img").remove("#checkmark");
-		$("td").animate({
-		opacity: 1}, 0, function(){});
+		blue = 0;
+		red = 0;
+		yellow = 0;
+		window.location = "#foo";
+		$("svg").remove();
+
 		setColor();
 	}
 
 	// making the pregame page not lose the color
-	$("#choice").click(function(){
+	$("h1").click(function(){
+		$("svg").remove();
+		setColor();
+		for (var i=0; i<board.length; i++) {
+				d3.select("#d"+i).style("opacity", 1).transition().duration(20000).style("opacity", 0);
+		}
 		console.log(color);
 	})
 
 	// function to get the color of a clicked cell
 	$("td").click(function() {
+		playSoundFx(SoundfxNum);
 		var color = $(this).css("background-color");
 		var choiceColor = $("#choice td").css("background-color");
-		makeShape('rect');
 		if (color === choiceColor) {
-			$(this).css({backgroundColor: '#FFFFFF'});
-			$(this).animate({
-			opacity: 1}, 0, function(){});
-			correct += 1;
+			d3.select(this).style("opacity", 0).transition().duration(0).style("opacity", 1);
 		}
+		if (color === "rgb(255, 255, 0)")
+			yellowClicked++;
+		if (color === "rgb(0, 0, 255)")
+			blueClicked++;
+		if (color === "rgb(255, 0, 0)")
+			redClicked++;
+		if (yellowClicked == (yellow*2) || blueClicked == (blue*2) || redClicked == (red*2)) {
+			for (var i=0; i<board.length; i++) {
+				d3.select("#d"+i).style("opacity", 0).transition().duration(0).style("opacity", 1);
+		}
+			nextLevel();
+		}
+
 		console.log(color);
+		console.log(yellowClicked);
 	})
 
-	$(".gameButton").click(function() {
-		$(".cell").fadeTo(5000, 0);
-	});
-
-	// clears grid once button is clicked
+	
 	$("#restart").click(function() {
 		reset();
 	});
@@ -95,44 +140,102 @@ $(document).ready(function() {
 			randomShapes(svgContainers[i]);
 		}	
 	}
-
+	
 	// function to randomly make a shape
 	function randomShapes(svg) {
-		var num = Math.floor(Math.random()*6)
-		var shape;
-		var color;
-		switch (num) {
-			case 0:
-			color = "green"; 
-				svg.append("circle")
-				.attr("cx",25)
-				.attr("cy",25)
-				.attr("r",25)
-				.attr("fill", color);
+	var num = Math.floor(Math.random()*6)
+	var shape;
+	var color;
+	switch (num) {
+		case 0:
+		color = "green"; 
+		svg.append("circle")
+		.attr("cx",25)
+		.attr("cy",25)
+		.attr("r",25)
+		.attr("fill", color);
+		break;
+		case 1: 
+		color = "cyan";
+		svg.append("rect")
+		.attr("width",50)
+		.attr("height",50)
+		.attr("fill", color);
+		break;
+		case 2: 
+		
+		break;
+		case 3: 
+		break;
+		case 4: 
+		break;
+		case 5: 
+		break;
+		default: ;
+		console.log("not a valid shape");
+	}
+}
+	//sound effects
+	function playSoundFx(fx){
+		switch(fx){
+			case "0":
+				SoundfxNum = "1";
+				fx1.play();
 				break;
-			case 1: 
-				color = "cyan";
-				svg.append("rect")
-				.attr("width",50)
-				.attr("height",50)
-				.attr("fill", color);
+			case "1" :
+				SoundfxNum = "2";
+				fx2.play();
 				break;
-			case 2: 
-
+			case  "2":
+				SoundfxNum = "3";
+				fx3.play();
 				break;
-			case 3: 
+			case  "3":
+				SoundfxNum = "4";
+				fx4.play();
 				break;
-			case 4: 
+			case  "4":
+				SoundfxNum = "0";
+				fx5.play();
 				break;
-			case 5: 
-				break;
-			default: ;
-				console.log("not a valid shape");
 		}
 	}
-	// fades game board timer set to 5 sec
-	setColor();
-	$(".cell").fadeTo(5000, 0);
-	//var svgContainer = d3.select("#d2").append("svg").attr("width", 50).attr("height", 50);
-	//var circle = svgContainer.append("circle").attr("cx", 30).attr("cy", 30).attr("r", 20);
+	// function that mute the sound 
+	$("#muteButton").click(function() {
+		if(mute == 0){
+			mute = 1;
+			document.getElementById("music").pause();
+			$(this).attr("src","images/mute.png");
+		} else {
+			mute = 0;
+			document.getElementById("music").play();
+			$(this).attr("src","images/sound.png");
+		}
+	});
+
+	// function that tally up all the colors
+	function countColor() {
+		color = [];
+		for (var i=0; i<board.length; i++) {
+			color[i] = $("#d" + i).css("background-color");
+			
+		}
+		for (var i=0; i<color.length; i++) {
+			if (color[i] === "rgb(0, 0, 255)")
+				blue++;
+			if (color[i] === "rgb(255, 0, 0)")
+				red++;
+			if (color[i] === "rgb(255, 255, 0)")
+				yellow++;
+
+		}
+
+	}
+
+	function nextLevel() {
+		window.location = "#clear"
+	}
+
+setColor();
+
 });
