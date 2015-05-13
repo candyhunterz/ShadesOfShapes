935c0d11b19edc843c501d79a341521da1367bcd
echlau <ericlau96@hotmail.com>
2015-05-13 13:19:51 -0700

    changed difficulty logic

--- js/difficulty.js
+++ js/difficulty.js
@@ -4,14 +4,32 @@ $(document).ready(function() {
 	var numShapes = 0;
 	var fadeTime = 18000;
 	
-	function difficulty(level) {
+	function startLevel(level) {
+		//TO-DO: put in start level
+		
 		if(level <= 7) {
-			numColors++;
+			//Easy Levels 1-7
+			if(numColors < 5) {
+				numColors++;
+			}
 		}else if(level <= 14) {
-			numShapes++;
+			//Medium Levels 8-14
+			if(numShapes < 6) {
+				numShapes++;
+			}
 		}else {
-			fadeTime -= 500;
+			//Hard Levels 15+
+			if(fadeTime > 5000) {
+				fadeTime -= 500;
+			}
 		}
+	}
+	
+	function newGame() {
+		gameLevel = 1;
+		numColors = 2;
+		numShapes = 0;
+		fadeTime = 18000;
 	}
 	
 });
