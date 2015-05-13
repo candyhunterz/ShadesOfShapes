67744ad3039b332e9d454b781339cc90559efb6c
Nguyen <nguyenproas@hotmail.com>
2015-05-13 12:48:31 -0700

    changed random color

--- js/shades.js
+++ js/shades.js
@@ -43,7 +43,7 @@ $(document).ready(function() {
 
 	//function to randomize the colors of the cell
 
-	function randomize(color) {
+	function randomize() {
 		for (var i=0; i<board.length; i++) {
 			var num = Math.floor(Math.random()*3)
 			var color='';
@@ -153,22 +153,22 @@ $(document).ready(function() {
 	function randomShapes(svg) {
 	var num = Math.floor(Math.random()*6)
 	var shape;
-	var color;
+	var color = [];
 	switch (num) {
 		case 0:
-		color = "green"; 
+		color = randomColor(1); 
 		svg.append("circle")
 		.attr("cx",25)
 		.attr("cy",25)
 		.attr("r",25)
-		.attr("fill", color);
+		.attr("fill", color[0]);
 		break;
 		case 1: 
-		color = "cyan";
+		color = randomColor(1);
 		svg.append("rect")
 		.attr("width",50)
 		.attr("height",50)
-		.attr("fill", color);
+		.attr("fill", color[0]);
 		break;
 		case 2: 
 		
@@ -243,6 +243,49 @@ $(document).ready(function() {
 	function nextLevel() {
 		window.location = "#clear"
 	}
+
+
+//////////////////////////////////////////////////////////////////////////////////
+
+	// new function to generate color
+	// cnum from 0 to 5
+	// returns an array of random colors
+	function randomColor(cnum) {
+		var colorList = [];
+		for (var i=0; i<cnum; i++) {
+			var num = Math.floor(Math.random()*5);
+			var color;
+			switch (num) {
+				case 0: 
+					color = "blue";
+					colorList[i] = color;
+					break;
+				case 1:
+					color = "red";
+					colorList[i] = color;
+					break;
+				case 2:
+					color = "yellow";
+					colorList[i] = color;
+					break;
+				case 3:
+					color = "green";
+					colorList[i] = color;
+					break;
+				case 4:
+					color = "purple";
+					colorList[i] = color;
+					break;
+				default:
+					console.log("cnum too big");
+			}
+
+		}	
+		//console.log(colorList);
+		
+		return colorList;
+	}
+	
 
 
 });
