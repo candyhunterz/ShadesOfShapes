7f3331c06ed876ad6a8b251bdf7e679d43d85f8e
peymantp <peymantparsa@gmail.com>
2015-05-19 12:19:04 -0700

    Added functions for caching

--- js/peyman.js
+++ js/peyman.js
@@ -37,4 +37,44 @@ $(document).ready(function() {
 			});
 		}
 	});
+	
+	function setCookie(cname, cvalue, exdays) {
+		var d = new Date();
+		d.setTime(d.getTime() + (exdays*24*60*60*1000));
+		var expires = "expires="+d.toUTCString();
+		document.cookie = cname + "=" + cvalue + "; " + expires;	
+	}
+	/*	
+	Take the cookiename as parameter (cname).
+	Create a variable (name) with the text to search for (cname + "=").
+	Split document.cookie on semicolons into an array called ca (ca = document.cookie.split(';')).
+	Loop through the ca array (i=0;i<ca.length;i++), and read out each value c=ca[i]).
+	If the cookie is found (c.indexOf(name) == 0), return the value of the cookie (c.substring(name.length,c.length).
+	If the cookie is not found, return "".
+	*/
+	function getCookie(cname) {
+		var name = cname + "=";
+		var ca = document.cookie.split(';');
+		for(var i=0; i<ca.length; i++) {
+			var c = ca[i];
+			while (c.charAt(0)==' ') 
+				c = c.substring(1);
+			if (c.indexOf(name) == 0) 
+				return c.substring(name.length, c.length);
+		}
+		return "";
+	}
+
+	//example on making a cookie and checking for it
+	function checkCookie() {
+		var user = getCookie("username");
+		if (user != "") {
+			alert("Welcome again " + user);
+		} else {
+			user = prompt("Please enter your name:", "");
+			if (user != "" && user != null) {
+				setCookie("username", user, 365);
+			}
+		}
+	}
 });
