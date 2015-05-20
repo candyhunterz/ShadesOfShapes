a943acc334705a5144a07d2d45c288a6b6a52efe
Nguyen <nguyenproas@hotmail.com>
2015-05-20 02:21:13 -0700

    added another shape + first achievement

--- js/shades.js
+++ js/shades.js
@@ -10,6 +10,7 @@ $(document).ready(function() {
 	var fx3 = new Audio("sounds/fx3.mp3");
 	var fx4 = new Audio("sounds/fx4.mp3");
 	var fx5 = new Audio("sounds/fx5.mp3");
+	var achv = new Audio("sounds/AchievementUnlocked.mp3");
 	var correct = 0;
 	var correctClicked = 0;
 	var clicked = false;
@@ -44,6 +45,7 @@ $(document).ready(function() {
 	function startLevel() {
 		// starts level
 		setColor();
+		
 		hasShape = $.contains("#d25" ,"svg");
 		console.log(hasShape);
 		if(gameLevel <= 5) {
@@ -53,6 +55,7 @@ $(document).ready(function() {
 			}
 		}else if(gameLevel <= 10) {
 			//Medium Levels 8-14
+			
 			randomChoice(2);
 			makeShapes(2);
 
@@ -172,6 +175,7 @@ $(document).ready(function() {
 				d3.select("#d"+i).style("opacity", 0).transition().duration(0).style("opacity", 1);
 			}
 			nextLevel();
+			achievements();
 		}
 
 		
@@ -212,12 +216,12 @@ $(document).ready(function() {
 			switch (num) {
 				case 0:
 				color = randomColor(1); 
-				shape = svg.append("circle")
+				svg.append("circle")
 				.attr("cx",25)
 				.attr("cy",25)
 				.attr("r",25)
 				.attr("fill", color[0]);
-				shapeList[i] = shape;
+				shapeList[i] = "circle";
 				return "circle";
 				break;
 				case 1: 
@@ -226,10 +230,14 @@ $(document).ready(function() {
 				.attr("width",50)
 				.attr("height",50)
 				.attr("fill", color[0]);
-				shapeList[i] = shape;
+				shapeList[i] = "rect";
 				return "rect";
 				break;
 				case 2: 
+				color = randomColor(1);
+				svg.append("polygon").attr("points", "25, 0 0, 50 50,50").attr("fill", color[0]);
+				shapeList[i] = "triangle";
+				return "triangle";
 				break;
 				case 3: 
 				break;
@@ -295,7 +303,7 @@ $(document).ready(function() {
 		$("#levelClear").html("LEVEL " + gameLevel + " CLEARED");
 		if (fadeTime > 5000)
 			fadeTime -= 500;
-		gameLevel++;
+		
 		
 		
 		
@@ -305,6 +313,7 @@ $(document).ready(function() {
 			$("#d"+i).removeClass().addClass("gametd");
 		}
 		window.location = "#clear"
+		gameLevel++;
 		$(".score").html(score);
 	}
 
@@ -427,5 +436,19 @@ $(document).ready(function() {
 				setCookie("username", user, 365);
 			}
 		}
+	}
+
+	// ---------------------------------------------------
+	// ACHIEVEMENTS //
+	function achievements() {
+		
+
+		
+			achv.play();
+			$("#ac").prepend('<img id="ach1" src="images/a1.png" />').hide();
+			$("#ac").show().slideDown("slow", function() {
+				$("#ach1").fadeOut(5000);
+			});
+		
 	}
 });
