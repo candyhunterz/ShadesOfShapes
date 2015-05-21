eae08d84cb0d55dcb790d0fd1b472ded56a8617e
Ycasas-Jan <ycasas.jan@gmail.com>
2015-05-20 20:16:45 -0700

    got the master version

--- js/shades.js
+++ js/shades.js
@@ -3,7 +3,6 @@ $(document).ready(function() {
 	// init game constants
 	var board = [[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]];
 	var row1, row2, row3, row4, row5, col1, col2, col3, col4, col5, leftDiag, rightDiag;
-	var correct = 0;
 	var mute = 0;
 	var SoundfxNum = "0";
 	var fx1 = new Audio("sounds/fx1.mp3");
@@ -11,17 +10,24 @@ $(document).ready(function() {
 	var fx3 = new Audio("sounds/fx3.mp3");
 	var fx4 = new Audio("sounds/fx4.mp3");
 	var fx5 = new Audio("sounds/fx5.mp3");
+	var achv = new Audio("sounds/AchievementUnlocked.mp3");
 	var correct = 0;
 	var correctClicked = 0;
 	var clicked = false;
+	var hasShape = false;
 	var score = 0;
 	var gameLevel = 1;
 	var numColors = 3;
 	var numShapes = 0;
 	var fadeTime = 10000;
+	var faded = false;
 	var time = 0;
 	var stopWatch;
 	var lives = 3;
+	var achiev1 = false;
+	var achiev2 = false;
+	var achieve3 = false;
+	var bestScore = 0;
 
 	function startTime(){
 		stopWatch = setInterval(function(){
@@ -36,12 +42,12 @@ $(document).ready(function() {
 		$("#timer").TimeCircles().destroy();
 		$("#timer").TimeCircles();
 		var delay = 3800; //Your delay in milliseconds
+		$(".score").html(score);
 		$(".level").html(gameLevel);
 		$("#gameLevel").html(gameLevel);
-		$(".score").html(score);
 		startLevel();
 		setTimeout(function() {
-			window.location = "#game";
+			window.location = "#game"; 
 			if(stopWatch != null){
 				clearInterval(stopWatch);
 				time = 0;
@@ -52,6 +58,8 @@ $(document).ready(function() {
 				.duration(fadeTime).style("opacity", 0);
 			}
 			}, delay)
+		faded = true;
+
 	}
 	
 	function startLevel() {
@@ -76,6 +84,8 @@ $(document).ready(function() {
 				fadeTime -= 500;
 			}
 		}
+		hasShape = ($("#d25").hasClass("svg")?true:false);
+		countColor();
 	}
 	//Use the class tag so every Play and Playagain button can be referenced the same
 	$(".play").on('click touchstart', function(){
@@ -90,9 +100,13 @@ $(document).ready(function() {
 	// function to randomize the choice cell
 	function randomChoice(num) {
 		svgContainer = d3.select("#d25").append("svg")
-			.attr("width", 50)
-			.attr("height", 50);
-			randomShapes(svgContainer, num);
+			.attr("width", 52)
+			.attr("height", 52);
+			var shapeType = randomShapes(svgContainer, num)
+			if(shapeType) {
+				$("#d25").addClass(shapeType);
+				$("#d25").addClass("svg");
+			}
 
 	}
 
@@ -100,7 +114,7 @@ $(document).ready(function() {
 
 	function randomize(cnum) {
 			for (var i=0; i<board.length; i++) {
-			var num = Math.floor(Math.random()*3)
+			var num = Math.floor(Math.random()*cnum)
 			var color='';
 			board[i][0] = 0;
 			if (num == 0) {
@@ -129,7 +143,6 @@ $(document).ready(function() {
 	function setColor() {
 		randomize(numColors);
 		//makeShapes();
-		countColor();
 	}
 	
 	//function to return the color of selection box
@@ -168,7 +181,7 @@ $(document).ready(function() {
 					var choiceColor = $("#choice td").css("background-color");
 					var choiceSVG = d3.select("svg");
 					var colorSVG = $(this).css("fill");
-					if (color === choiceColor) {
+					if (color === choiceColor && (!hasShape && (this.className == "gametd svg" || this.className == "gametd") || $("#d"+i).hasClass($("#d25").attr("class")))) {
 						d3.select(this).style("opacity", 0).transition().duration(0).style("opacity", 1);
 						var timeDifference = (fadeTime - time)/1000;
 						score = score + ((gameLevel * 10)  * timeDifference) + 100;
@@ -177,7 +190,10 @@ $(document).ready(function() {
 					} else {
 						lives--;
 						if(lives <= 0){
-							window.location="#done";
+							window.location = "#done";
+							if(score > bestScore){
+								$("#bestScore").text(score);
+							}
 							lives = 3;
 						}
 						$("#gameLives").text(lives);
@@ -189,6 +205,7 @@ $(document).ready(function() {
 			for (var i=0; i<board.length; i++) {
 				d3.select("#d"+i).style("opacity", 0).transition().duration(0).style("opacity", 1);
 			}
+			achievements();
 			nextLevel();
 		}
 
@@ -211,10 +228,10 @@ $(document).ready(function() {
 		var shape;
 		// making svg containers in all squares
 		for (var i=0; i<board.length; i++) {
-			svgContainers[i] = d3.select("#d"+i).append("svg")
-			.attr("width", 50)
-			.attr("height", 50);
-			randomShapes(svgContainers[i], num);
+			svgContainers[i] = d3.select("#d"+i).append("svg");
+			var shapeType = randomShapes(svgContainers[i], num);
+			$("#d"+i).addClass(shapeType);
+			$("#d"+i).addClass("svg");
 		}	
 	}
 	
@@ -230,21 +247,37 @@ $(document).ready(function() {
 				case 0:
 				color = randomColor(1); 
 				shape = svg.append("circle")
-				.attr("cx",25)
-				.attr("cy",25)
+				.attr("cx",27)
+				.attr("cy",27)
 				.attr("r",25)
+				.attr("stroke", "turquoise")
+				.attr("stroke-width" , 3)
 				.attr("fill", color[0]);
 				shapeList[i] = shape;
+				return "circle";
 				break;
 				case 1: 
 				color = randomColor(1);
 				shape = svg.append("rect")
+				.attr("x", 2)
+				.attr("y", 2)
 				.attr("width",50)
 				.attr("height",50)
+				.attr("stroke", "turquoise")
+				.attr("stroke-width" , 3)
 				.attr("fill", color[0]);
 				shapeList[i] = shape;
+				return "rect";
 				break;
 				case 2: 
+				color = randomColor(1);
+				svg.append("polygon")
+				.attr("points", "25, 0 0, 50 50,50")
+				.attr("stroke", "turquoise")
+				.attr("stroke-width" , 3)
+				.attr("fill", color[0]);
+				shapeList[i] = "triangle";
+				return "triangle";
 				break;
 				case 3: 
 				break;
@@ -256,8 +289,8 @@ $(document).ready(function() {
 				console.log("not a valid shape");
 			}
 		}
-		console.log(shapeList);
-		return shapeList;
+		//console.log(shapeList);
+		//return shapeList;
 }
 	//sound effects
 	function playSoundFx(fx){
@@ -300,13 +333,14 @@ $(document).ready(function() {
 	// function that tally up all the colors
 	function countColor() {
 		for (var i=0; i<board.length; i++) {
-			if($("#d" + i).css("background-color") == $("#d25").css("background-color")) {
+			if($("#d" + i).css("background-color") == $("#d25").css("background-color") && (!hasShape && (document.getElementById("d"+i).className == "gametd svg" || document.getElementById("d"+i).className == "gametd") || $("#d"+i).hasClass($("#d25").attr("class")))) {
 				correct++;
 			}
 		}
 	}
 
 	function nextLevel() {
+		faded = false;
 		$("#levelClear").html("LEVEL " + gameLevel + " CLEARED");
 		if (fadeTime > 5000)
 			fadeTime -= 500;
@@ -317,8 +351,12 @@ $(document).ready(function() {
 		for(var i=0; i<board.length; i++) {
 			//Sets all cells to be able for clicking again.
 			board[i][1] =0;
+			$("#d"+i).removeClass().addClass("gametd");
 		}
+		$("#d25").removeClass();
 		window.location = "#clear"
+		//gameLevel++;
+		
 		$(".score").html(score);
 	}
 
@@ -359,7 +397,7 @@ $(document).ready(function() {
 			}
 
 		}	
-		console.log(colorList);
+		//console.log(colorList);
 		
 		return colorList;
 	}
@@ -370,15 +408,13 @@ $(document).ready(function() {
 		window.location = "#main";
 	})
 
-	$(".sumbit").click(function(){
-		window.location = "#sumbitPage";
-		$("#scoreSumbit").text(score);
+	$(".board").click(function(){
+		window.location = "#LeaderboardPage";
 	})
 
 	var name,num;
-	$("#sumbitButton").click(function(){
-		window.location = "#LeaderboardPage";
-		var num = score;
+	$("#send").click(function(){
+		var num = parseInt($('#userScore').val());
 		var user = $('#userID').val();
 		console.log(num);
 		$.ajax({
@@ -444,14 +480,65 @@ $(document).ready(function() {
 			}
 		}
 	}
+	// ---------------------------------------------------
+	// ACHIEVEMENTS //
+	function achievements() {
+		
+		if (gameLevel == 3 && achiev1 == false) {
+			achiev1 = true;
+			achv.play();
+			$("#ac").prepend('<img id="ach1" src="images/a1.png" />').hide();
+			$("#ac").show().slideDown("slow", function() {
+				$("#ach1").fadeOut(5000, function() {$("img#ach1").remove();});
+			});
+			
+		}
 
+		if (time >= fadeTime && achiev2 == false) {
+			achiev2 = true;
+			achv.play();
+			$("#ac").prepend('<img id="ach2" src="images/a2.png" />').hide();
+			$("#ac").show().slideDown("slow", function() {
+				$("#ach2").fadeOut(5000, function() {$("img#ach1").remove();});
+			});
+			
+		}
+
+	}
+	
 	$("#achievementsButton").click(function(){
 		window.location="#achievements";
 	});
 
 	$("#leaderBoard").click(function(){
 		window.location="#LeaderboardPage";
+	});
+
+	$(".settingButton").click(function(){
+		window.location = "#setting";
 	});	
 
+	$(".submit").click(function(){
+		window.location = "#sumbitPage";
+		$("#scoreSumbit").text(score);
+	})
+
+	var name,num;
+	$("#sumbitButton").click(function(){
+		window.location = "#LeaderboardPage";
+		var num = score;
+		var user = $('#userID').val();
+		console.log(num);
+		$.ajax({
+			url: "https://api.mongolab.com/api/1/databases/sos/collections/leaderboard?apiKey=br10X-RgokMGFuGnyr5w4WHdKpa046Fr",
+			type: "POST",
+			data: JSON.stringify({name: user, score: num}),
+			contentType: "application/json"
+			}).done(function( msg ) {
+			console.log(msg);
+		});	
+		console.log(user);
+	}); 
 
 });
+
