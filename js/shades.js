517ab4060a958cee9d8ecd9357101d97705c3fc9
echlau <ericlau96@hotmail.com>
2015-05-21 14:32:59 -0700

    some code cleanup

--- js/shades.js
+++ js/shades.js
@@ -188,20 +188,8 @@ $(document).ready(function() {
 		
 	}
 
-	// making the pregame page not lose the color
-	/* 
-	$("h1").on('click touchstart', function(){
-		$("svg").remove();
-		setColor();
-		for (var i=0; i<board.length; i++) {
-				d3.select("#d"+i).style("opacity", 1).transition().duration(20000).style("opacity", 0);
-		}
-		console.log(color);
-	}) */
-
 	// function to get the color of a clicked cell
 	$("td.gametd").on('click touchstart', function() {
-		console.log(this.id);
 		if(this.id != "d25"){ //Can't click cell in pre-game page
 			for(var i=0; i<board.length; i++) {
 				//Each cell can only be clicked once
@@ -320,9 +308,8 @@ $(document).ready(function() {
 				console.log("not a valid shape");
 			}
 		}
-		//console.log(shapeList);
-		//return shapeList;
-}
+	}
+
 	//sound effects
 	function playSoundFx(fx){
 		switch(fx){
@@ -430,9 +417,7 @@ $(document).ready(function() {
 					console.log("cnum too big");
 			}
 
-		}	
-		//console.log(colorList);
-		
+		}
 		return colorList;
 	}
 
@@ -451,16 +436,13 @@ $(document).ready(function() {
 			 $('#userID').attr("placeholder",'Name can not be empty');
 			 return;
 		}
-		console.log(num);
 		$.ajax({
 			url: "https://api.mongolab.com/api/1/databases/sos/collections/leaderboard?apiKey=br10X-RgokMGFuGnyr5w4WHdKpa046Fr",
 			type: "POST",
 			data: JSON.stringify({name: user, score: num}),
 			contentType: "application/json"
 			}).done(function( msg ) {
-			console.log(msg);
 		});	
-		console.log(user);
 		window.location = "#main";
 	}); 
 
